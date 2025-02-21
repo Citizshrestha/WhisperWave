956b5fddfd4c54e1f26509e5c3ba88d19e2e1564
Citizshrestha <citizshresthaa@gmail.com>
2025-02-21 11:09:01 +0545

    search modal

--- src/components/SearchModal.jsx
+++ src/components/SearchModal.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+// import React from 'react'
+
+import { RiSearchLine } from "react-icons/ri"
 
 const SearchModal = () => {
   return (
-    <div>SearchModal</div>
+    <div>
+       <button className="bg-[#D9F2ED] w-[2.2rem] p-2 flex items-center justify-center rounded-lg ">
+        <RiSearchLine color="#01AA85" className="w-[1.13rem] h-[1.13rem] "/>
+       </button>
+    </div>
   )
 }
 
