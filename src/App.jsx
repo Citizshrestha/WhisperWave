43df54da1aa3813aa932ed2c6d51713cb14ba0a1
Citizshrestha <citizshresthaa@gmail.com>
2025-02-24 17:26:42 +0545

    firebase authentication for login/signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
+import { useState , useEffect } from 'react';
 import Navlink from "./components/Navlink";
 import ChatList from "./components/ChatList";
 import Chatbox from "./components/Chatbox";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import {auth} from "./firebase/firebase";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    const currentUser = auth.currentUser;
+    console.log(currentUser);
+     if(currentUser){
+      setUser(currentUser)
+     }
+
+     const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user);
+     });
+
+     return () => unsubscribe();
+
+  }, []);
+
+
+
   return (
     <div>
-        <div className="flex lg:flex-row flex-col items-start w-full ">
-          <Navlink />
-          <ChatList />
-          <Chatbox />
-        </div>
-              <div>
-                {/* <Login/>  */}
-                {/* <Register/> */}
-              </div>
+      {user ? (
+         <div className="flex lg:flex-row flex-col items-start w-full ">
+         <Navlink />
+         <ChatList />
+         <Chatbox />
+       </div>
+      ) : (
+        <div>{isLogin ?  <Login isLogin={isLogin} setIsLogin={setIsLogin}/> : <Register isLogin={isLogin} setIsLogin={setIsLogin}/>}</div>
+      )};     
     </div>
   );
 };
