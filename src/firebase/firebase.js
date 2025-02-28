89f1fdb9d9b434dcdae7371b5d0f7d5955cd645e
Citizshrestha <citizshresthaa@gmail.com>
2025-02-28 16:51:27 +0545

    Add .env to .gitignore for security

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,18 @@
+
+import dotenv from 'dotenv';
+dotenv.config();
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, onSnapshot,doc,setDoc,getDoc, serverTimestamp, updateDoc, addDoc, Timestamp } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
+
 const firebaseConfig = {
-  apiKey: "AIzaSyA243zxci9wJjwXwZ9ADQDtrvGDUs0Ub0Y",
-  authDomain: "chat-application-21466.firebaseapp.com",
-  projectId: "chat-application-21466",
-  storageBucket: "chat-application-21466.firebasestorage.app",
-  messagingSenderId: "739884461695",
-  appId: "1:739884461695:web:6e4493c5186bb3b639594a",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
